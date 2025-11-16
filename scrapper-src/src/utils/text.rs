f38f3b66bf856928cc57f0db6b50eb206232199e
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2025-11-16 15:31:41 +0100

    feat: add clean_text_html()

--- scrapper-src/src/utils/text.rs
+++ scrapper-src/src/utils/text.rs
@@ -7,3 +7,12 @@ pub fn clean_text(a: &scraper::ElementRef<'_>) -> String {
         .trim()
         .to_string()
 }
+
+pub fn clean_text_html(a: &String) -> String {
+    a.trim()
+        .replace('\n', " ")
+        .replace(char::is_whitespace, " ")
+        .split_whitespace()
+        .collect::<Vec<_>>()
+        .join(" ")
+}
