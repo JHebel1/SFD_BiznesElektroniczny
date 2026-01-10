2f9b73e93ba995efa8bec5b9524cf45019ba8118
JHebel1 <jakubhebelll@gmail.com>
2026-01-10 21:20:48 +0100

    fix: translations

---
+++ shop-src/prestashop/modules/poscompare/translations/pl.php
@@ -0,0 +1,19 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{poscompare}prestashop>poscompare_bb128817146223acd53d0cbf45beb579'] = 'Porównywarka';
+$_MODULE['<{poscompare}prestashop>poscompare_497fa615ee083e46ef28a96310fb147e'] = 'Zezwól klientom na porównywanie produktów ';
+$_MODULE['<{poscompare}prestashop>poscompare_b6e4507555ebfe6bd4b27b4cd3a95ebb'] = 'Dodano produkt do porównania';
+$_MODULE['<{poscompare}prestashop>poscompare_45552b6720fce18f5b30b504b5b28c11'] = 'Wyświetl porównywane produkty';
+$_MODULE['<{poscompare}prestashop>actions_65c727cbb872ca158a7b1b5ee7b7fa48'] = 'Dodano produkt do porównywarki';
+$_MODULE['<{poscompare}prestashop>actions_3d8073b0883c13a488004db4d16c204f'] = 'Produkt usunięto';
+$_MODULE['<{poscompare}prestashop>actions_c11e9db0f2e51467556def90d35c33ad'] = 'Wszystko produkty usunięto';
+$_MODULE['<{poscompare}prestashop>compare_page_ed203863f0a4ac73f521ec8f235453a8'] = 'Porównanie produktów';
+$_MODULE['<{poscompare}prestashop>compare_page_14380cb5fb4d995a5815e694013061a7'] = 'Usuń wszystkie produkty';
+$_MODULE['<{poscompare}prestashop>compare_page_ff9002208139901e2667d58c81d6f189'] = 'Brak atrybutów do porównania';
+$_MODULE['<{poscompare}prestashop>compare_page_8bd65573745ac01c77e8d5d21f9cc713'] = 'Nie ma produktów do porównania';
+$_MODULE['<{poscompare}prestashop>product_1063e38cb53d94d386f21227fcd84717'] = 'Usuń';
+$_MODULE['<{poscompare}prestashop>compare-success_b6e4507555ebfe6bd4b27b4cd3a95ebb'] = 'Produkt dodano do porównania';
+$_MODULE['<{poscompare}prestashop>compare_top_7eece51cf3938103677db7a5051ef8f5'] = 'Porównaj';
+$_MODULE['<{poscompare}prestashop>product-page_aa1411cc44ecfafbca0d71427fe3dc7a'] = 'Dodaj do porównania';
---
+++ shop-src/prestashop/modules/posshoppingcart/translations/pl.php
@@ -0,0 +1,14 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_cd8054a8310459bee1ef7586a83cce5b'] = 'Koszyk';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_e03093a5753b436ee1de63b6e3e1bd02'] = 'Dodaje blok zawierający koszyk klienta.';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_a21e5718d2a196280b729438933501c7'] = 'Ajax: zły wybór';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_f4f70727dc34561dfde1a3c529b6205c'] = 'Ustawienia';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_614a8820aa4ac08ce2ee398a41b10778'] = 'Ajax koszyk';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_eefd19ecf1f6d94a308dcfc95981bbf9'] = 'Aktywuj tryb Ajax dla koszyka (zgodny z domyślnym motywem).';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Włączony';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_b9f5c797ebbf55adccdd8539a65a0241'] = 'Wyłączony';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_d4c3503f25d4a095d5c9573a26c751c7'] = 'Wygląd koszyka';
+$_MODULE['<{posshoppingcart}prestashop>posshoppingcart_c9cc8cce247e49bae79f15173ce97354'] = 'Zapisz';
